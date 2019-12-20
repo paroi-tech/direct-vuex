26011f422b1d3b9c799aa7d0b1929a7e0fb4ed89
Paleo <paleo@paleo.casa>
2019-12-20 15:09:00 +0100

    Use rollup

--- build/make-bundles.js
+++ build/make-bundles.js
@@ -1,22 +1,34 @@
 const { existsSync } = require("fs")
 const { readFile, writeFile, mkdir } = require("fs").promises
 const { join, resolve } = require("path")
+const { rollup } = require("rollup")
 const terser = require("terser")
 
 const packagePath = resolve(__dirname, "..")
-const compiledEsmPath = join(__dirname, "compiled-esm")
-const compiledUmdPath = join(__dirname, "compiled-umd")
 const distNpmPath = join(packagePath, "dist")
 
 async function build() {
   if (!existsSync(distNpmPath))
     await mkdir(distNpmPath)
-  await buildMainFile(join(compiledEsmPath, "direct-vuex.js"), "direct-vuex.esm")
-  await buildMainFile(join(compiledUmdPath, "direct-vuex.js"), "direct-vuex.umd")
+  await makeBundle(join(__dirname, "compiled-esm", "direct-vuex.js"), "direct-vuex.esm", "esm")
+  await makeBundle(join(__dirname, "compiled-esm", "direct-vuex.js"), "direct-vuex.umd", "umd")
+  await makeBundle(join(__dirname, "compiled-esm", "direct-vuex.js"), "direct-vuex.system", "system")
 }
 
-async function buildMainFile(mainFile, bundleName) {
-  const bundleCode = await readFile(mainFile, "utf-8")
+async function makeBundle(mainFile, bundleName, format) {
+  const bundle = await rollup({ input: mainFile })
+  const { output } = await bundle.generate({
+    format,
+    sourcemap: false,
+    output: {
+      name: "direct-vuex",
+      exports: "named"
+    },
+    globals: {
+      vuex: "Vuex"
+    },
+  })
+  const bundleCode = output[0].code
   const minified = terser.minify({
     bundle: bundleCode
   })
